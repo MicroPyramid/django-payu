78f45499c4bc73a8d66536abbfcace6deda520f7
Ashwin Kumar <ashwin@micropyramid.com>
2015-08-22 16:25:42 +0530

    fixed docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
-# django-payu documentation build configuration file, created by
-# sphinx-quickstart on Sat Aug 22 16:05:40 2015.
+# pietrack documentation build configuration file, created by
+# sphinx-quickstart on Wed Jul 29 18:10:32 2015.
 #
 # This file is execfile()d with the current directory set to its
 # containing dir.
@@ -29,7 +29,9 @@ import shlex
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -46,9 +48,9 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'django-payu'
-copyright = u'2015, Micropyramid'
-author = u'Micropyramid'
+project = u'django_payu'
+copyright = u'2015, Micro Pyramid'
+author = u'Micro Pyramid'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -200,7 +202,7 @@ html_static_path = ['_static']
 #html_search_scorer = 'scorer.js'
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'django-payudoc'
+htmlhelp_basename = 'django_payudoc'
 
 # -- Options for LaTeX output ---------------------------------------------
 
@@ -222,8 +224,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  (master_doc, 'django-payu.tex', u'django-payu Documentation',
-   u'Micropyramid', 'manual'),
+  (master_doc, 'django_payu.tex', u'django_payu Documentation',
+   u'Micro Pyramid', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -252,7 +254,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'django-payu', u'django-payu Documentation',
+    (master_doc, 'django_payu', u'django_payu Documentation',
      [author], 1)
 ]
 
@@ -266,8 +268,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  (master_doc, 'django-payu', u'django-payu Documentation',
-   author, 'django-payu', 'One line description of project.',
+  (master_doc, 'django_payu', u'django_payu Documentation',
+   author, 'django_payu', 'One line description of project.',
    'Miscellaneous'),
 ]
 
