3e5f510f2a01428f0643c24ac7228b747d645574
chaitanya <chaitanya@micropyramid.com>
2016-08-03 14:13:34 +0530

    updated to 0.2

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ for dirpath, dirnames, filenames in os.walk(PROJECT_NAME):
 
 setup(
     name='django-payu',
-    version='0.1',
+    version='0.2',
     packages=['payu'],
     include_package_data=True,
     description='A simple PayU app for Django.',
